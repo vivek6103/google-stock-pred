4e4488985c48bf181fe265faafb9c78022377255
[vivek mishra] <[mishravivekkumar974@gmail.com]>
2024-08-13 00:32:00 +0530

    req update

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ import streamlit as st
 import pickle
 import numpy as np
 import pandas as pd
+
+# Ignore the ComplexWarning
+
+
 # Load the trained model from the pickle file
 with open('model12.pkl', 'rb') as file:
     model = pickle.load(file)
